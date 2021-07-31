9ca86e8fc88c7f6e5369c2c82f149591a5cc5df8
a-sohrabi <sohrabi1372@gmail.com>
2021-08-01 00:03:34 +0430

    add vue.js for frontend

---
+++ vue/final_project_vue/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="wrapper">
+        <nav class="navbar is-dark">
+            <div class="navbar-brand">
+                <router-link to="/" class="navbar-item">
+                    <strong>Final project</strong>
+
+                </router-link>
+                <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}" >
+                <div class="navbar-end">
+                    <router-link to="/category1" class="navbar-item">category1</router-link>
+                    <router-link to="/category2" class="navbar-item">category2</router-link>
+
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <router-link to="/log-in" class="button is-light">Log in</router-link>
+
+                            <router-link to="/cart" class="button is-success">
+                                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                                <span>Cart</span>
+                            </router-link>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </nav>
+
+        <section class="section">
+            <router-view/>
+        </section>
+        <footer>
+            <p class="has-text-centered">Copyright (c) 2021</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                showMobileMenu: false,
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import '../node_modules/bulma';
+</style>
