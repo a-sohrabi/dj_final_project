4cd69134cd61ea052ee4ead8b417f4b58e32ad8e
a-sohrabi <sohrabi1372@gmail.com>
2021-08-07 18:59:34 +0430

    implement sign up

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -5,7 +5,9 @@ import Home from '../views/Home.vue'
 import Product from '../views/product.vue'
 import Category from '../views/category.vue'
 import Search from '../views/search.vue'
-import Cart from "../views/Cart.vue";
+import Cart from "../views/Cart.vue"
+import SignUp from "../views/SignUp.vue"
+
 
 const routes = [
     {
@@ -20,6 +22,11 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/sign-up',
+        name: 'SignUp',
+        component: SignUp
     },
     {
         path: '/search',
