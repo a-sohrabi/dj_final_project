4e2a55c0e17be79fe3c1ba47a451cd13e99df29c
a-sohrabi <sohrabi1372@gmail.com>
2021-08-09 15:00:41 +0430

    create my account page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -42,7 +42,14 @@
 
                     <div class="navbar-item">
                         <div class="buttons">
-                            <router-link to="/log-in" class="button is-light">Log in</router-link>
+                            <template v-if="$store.state.isAuthenticated">
+                                <router-link to="/my-account" class="button is-light">My account</router-link>
+
+                            </template>
+                            <template v-else>
+                                <router-link to="/log-in" class="button is-light">Log in</router-link>
+
+                            </template>
 
                             <router-link to="/cart" class="button is-success">
                                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,5 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
+import store from '../store'
+
 import Home from '../views/Home.vue'
 
 import Product from '../views/product.vue'
@@ -8,6 +10,7 @@ import Search from '../views/search.vue'
 import Cart from "../views/Cart.vue"
 import SignUp from "../views/SignUp.vue"
 import LogIn from "../views/LogIn.vue"
+import MyAccount from "../views/MyAccount";
 
 
 const routes = [
@@ -35,6 +38,14 @@ const routes = [
         component: LogIn
     },
     {
+        path: '/my-account',
+        name: 'MyAccount',
+        component: MyAccount,
+        meta: {
+            requireLogin: true
+        }
+    },
+    {
         path: '/search',
         name: 'Search',
         component: Search
@@ -60,6 +71,15 @@ const routes = [
 const router = createRouter({
     "history": createWebHistory(process.env.BASE_URL),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+        next({ name: 'LogIn', query: { to: to.path }});
+    } else {
+        next()
+
+    }
 })
 
 export default router
