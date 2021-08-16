e67ca08b0d93913915068468d6ce1b6bde7f96f7
a-sohrabi <sohrabi1372@gmail.com>
2021-08-16 15:44:21 +0430

    some changes to admin page

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+
 from .models import Category, Product
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Category)
+# admin.site.register(Product)
+
+
+class MyAdminSite(AdminSite):
+
+    site_title = 'Navan nuts Admin'
+
+    site_header = 'Administration'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price')
+    list_filter = ('price', 'category', )
+    list_editable = ('price',)
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
 
